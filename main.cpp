3c17aab9a2385d790846ff5074d2d0dc0b99c9e5
lefkovitzj <85521235+lefkovitzj@users.noreply.github.com>
2026-03-02 20:05:37 -0500

    Add help and version commands

--- main.cpp
+++ main.cpp
@@ -22,6 +22,14 @@ std::vector<std::string> command_prompt() {
     return args;
 }
 
+void help_menu() {
+    std::cout << "VCS - Help Menu";
+}
+void version_menu() {
+    std::cout << "VCS - Version" << VCS_VERSION_NUM << "\n";
+    std::cout << "Find the most up-to-date version of VCS at https://github.com/lefkovitzj/vcs\n";
+}
+
 int main() {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
     std::cout << "VCS - an open source version control system by Joseph Lefkovitz\n";
@@ -29,7 +37,13 @@ int main() {
     while (true) {
         std::vector<std::string> args = command_prompt();
         if (! args.empty()) {
-            if (args.at(0) == "exit") {
+            if (args.at(0) == "help") {
+                help_menu();
+            }
+            else if (args.at(0) == "version") {
+                version_menu();
+            }
+            else if (args.at(0) == "exit") {
                 break;
             }
         }
