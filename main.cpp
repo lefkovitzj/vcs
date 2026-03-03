0bb6abca5942e5442e9336997c2dbe40701afd78
lefkovitzj <85521235+lefkovitzj@users.noreply.github.com>
2026-03-02 20:05:03 -0500

    Add command prompt and exit command

--- main.cpp
+++ main.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
 
+const float VCS_VERSION_NUM = 0.1;
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+std::vector<std::string> command_prompt() {
+    std::string command;
+    std::vector<std::string> args;
+
+    // Prompt and read command.
+    std::cout << "\\> ";
+    std::getline(std::cin, command);
+
+    // Read args from command.
+    std::istringstream iss(command);
+    std::string arg;
+    while (iss >> arg) {
+        args.push_back(arg);
+    }
+    return args;
+}
+
 int main() {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    std::cout << "VCS - an open source version control system by Joseph Lefkovitz\n";
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    while (true) {
+        std::vector<std::string> args = command_prompt();
+        if (! args.empty()) {
+            if (args.at(0) == "exit") {
+                break;
+            }
+        }
     }
-
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
