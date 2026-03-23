37ec78b65fb283992a577be0fbff95bbd1cbb72e
lefkovitzj <85521235+lefkovitzj@users.noreply.github.com>
2026-03-23 11:23:19 -0400

    Convert to program args rather than internal cli

--- main.cpp
+++ main.cpp
@@ -4,16 +4,10 @@
 #include <vector>
 
 const float VCS_VERSION_NUM = 0.1;
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-std::vector<std::string> command_prompt() {
-    std::string command;
-    std::vector<std::string> args;
 
-    // Prompt and read command.
-    std::cout << "\\> ";
-    std::getline(std::cin, command);
-
+std::vector<std::string> parse_command(std::string command) {
     // Read args from command.
+    std::vector<std::string> args;
     std::istringstream iss(command);
     std::string arg;
     while (iss >> arg) {
@@ -30,24 +24,25 @@ void version_menu() {
     std::cout << "Find the most up-to-date version of VCS at https://github.com/lefkovitzj/vcs\n";
 }
 
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    std::cout << "VCS - an open source version control system by Joseph Lefkovitz\n";
+int main(int argc, char **argv) {
+    std::string command_str;
+    for (int i = 1; i < argc; i++) {
+        command_str = argv[i];
+    }
+    std::vector<std::string> args = parse_command(command_str);
 
-    while (true) {
-        std::vector<std::string> args = command_prompt();
-        if (! args.empty()) {
-            if (args.at(0) == "help") {
-                help_menu();
-            }
-            else if (args.at(0) == "version") {
-                version_menu();
-            }
-            else if (args.at(0) == "exit") {
-                break;
-            }
+    if (! args.empty() and args.size() == 1) {
+        if (args.at(0) == "help") {
+            help_menu();
+        }
+        else if (args.at(0) == "version") {
+            version_menu();
+        }
+        else if (args.at(0) == "exit") {
+            // Terminate the program.
+            return 0;
         }
     }
+    std::cout << "\n";
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
